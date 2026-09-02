271ed2e1a3183575fcf625dc8c5595020800e63b
Davis Franklin <97471058+Davis245@users.noreply.github.com>
2026-09-02 12:33:08 -0700

    Remove font build mock configuration

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,4 @@
-import path from "node:path";
-
 import type { NextConfig } from "next";
-
-process.env.NEXT_FONT_GOOGLE_MOCKED_RESPONSES ??= path.join(
-  process.cwd(),
-  "lib/next-font-google-mocked-responses.cjs",
-);
 
 const nextConfig: NextConfig = {};
 
